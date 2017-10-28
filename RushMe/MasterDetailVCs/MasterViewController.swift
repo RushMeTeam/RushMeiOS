d900e4467223015c4228db3a23bf59a8466db0d9
Adam K <adamthk@gmail.com>
2017-10-28 14:07:25 -0400

    Favorites!
    Favorites have come to RushMe! Tap the little heart icon on the list of fraternities to see your favorites, and add new ones by swiping left on each fraternity!

--- RushMe/MasterDetailVCs/MasterViewController.swift
+++ RushMe/MasterDetailVCs/MasterViewController.swift
@@ -19,14 +19,20 @@ class MasterViewController : UITableViewController {
   
   // The hard data used in the table
   var objects = [Any]()
-  var fratNames = [String]
-
+  var fratNames = [String]()
   var fraternities = Dictionary<String, Fraternity>()
   // The menu button used to toggle the slide-out menu
   @IBOutlet var openBarButtonItem: UIBarButtonItem!
   // MARK: SQL
   let sqlHandler = SQLHandler()
+  var viewingFavorites = false
+  var favoritedFrats = [String]()
   
+  @IBAction func favoritesToggled(_ sender: UIBarButtonItem) {
+    viewingFavorites = !viewingFavorites
+    refreshControl?.isEnabled = !viewingFavorites
+    self.tableView.reloadData()
+  }
   override func viewDidLoad() {
     super.viewDidLoad()
     if (!COLOR_CONST.SLIDEOUT_MENU_SHADOW_ENABLED) {
@@ -98,6 +104,7 @@ class MasterViewController : UITableViewController {
           if let _ = image {
             frat.setProperty(named: "coverImage", to: UIImage(named: "rushCalendar.jpg")!)
           }
+          frat.setProperty(named: "favorited", to: false)
           fraternities[name] = frat
           fratNames.append(name)
         }
@@ -125,7 +132,11 @@ class MasterViewController : UITableViewController {
     // Checks if segue is going into detail
     if segue.identifier == "showDetail" {
         if let indexPath = tableView.indexPathForSelectedRow {
-          if let object = fraternities[fratNames[indexPath.row]] {
+          var fratName = fratNames[indexPath.row]
+          if (viewingFavorites) {
+            fratName = favoritedFrats[indexPath.row]
+          }
+          if let object = fraternities[fratName] {
             let controller = (segue.destination as! UINavigationController).topViewController
                                                                       as! DetailViewController
             // Send the detail controller the fraternity we're about to display
@@ -141,7 +152,11 @@ class MasterViewController : UITableViewController {
         else if let cell = sender as? FratCell {
           // Determine which object user selected
           if let indexPath = tableView.indexPath(for: cell) {
-            if let object = fraternities[fratNames[indexPath.row]] {
+            var fratName = fratNames[indexPath.row]
+            if (viewingFavorites) {
+              fratName = favoritedFrats[indexPath.row]
+            }
+            if let object = fraternities[fratName] {
               let controller = (segue.destination as! UINavigationController).topViewController
                                                                           as! DetailViewController
               controller.selectedFraternity = object
@@ -159,30 +174,110 @@ class MasterViewController : UITableViewController {
   // Should always be the number of objects to display
   override func tableView(_ tableView: UITableView,
                           numberOfRowsInSection section: Int) -> Int {
+    if (viewingFavorites) {
+      return favoritedFrats.count
+    }
     return max(fratNames.count, 1)
   }
 
   override func tableView(_ tableView: UITableView,
                           cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    if fratNames.count == 0 {
+    if (viewingFavorites) {
+      if (favoritedFrats.count == 0){
         let cell = UITableViewCell()
         cell.selectionStyle = .none
         cell.textLabel?.textAlignment = NSTextAlignment.center
-        cell.textLabel?.text = "Pull to Refresh!"
+        cell.textLabel?.text = "No Favorites!"
         cell.textLabel?.textColor = COLOR_CONST.MENU_COLOR
         return cell
+      }
+      let cell = tableView.dequeueReusableCell(withIdentifier: "FratCell") as! FratCell
+      if let frat = fraternities[favoritedFrats[indexPath.row]] {
+        cell.titleLabel!.text = frat.name
+        cell.subheadingLabel!.text = frat.chapter
+        cell.previewImageView!.image = frat.previewImage
+      }
+      return cell
     }
-    let cell = tableView.dequeueReusableCell(withIdentifier: "FratCell") as! FratCell
-    if let frat = fraternities[fratNames[indexPath.row]] {
-      cell.titleLabel!.text = frat.name
-      cell.subheadingLabel!.text = frat.chapter
-      cell.previewImageView!.image = frat.previewImage
+    else {
+      if fratNames.count == 0 {
+          let cell = UITableViewCell()
+          cell.selectionStyle = .none
+          cell.textLabel?.textAlignment = NSTextAlignment.center
+          cell.textLabel?.text = "Pull to Refresh!"
+          cell.textLabel?.textColor = COLOR_CONST.MENU_COLOR
+          return cell
+      }
+      let cell = tableView.dequeueReusableCell(withIdentifier: "FratCell") as! FratCell
+      if let frat = fraternities[fratNames[indexPath.row]] {
+        cell.titleLabel!.text = frat.name
+        cell.subheadingLabel!.text = frat.chapter
+        cell.previewImageView!.image = frat.previewImage
+      }
+      return cell
     }
-    return cell
   }
   
   @objc func handleRefresh(refreshControl : UIRefreshControl) {
       dataUpdate()
   }
+  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    // Should only be able to do things to cells if there are actually fraternities represented
+    return !fratNames.isEmpty
+  }
+  
+  override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+    var title = "Favorite"
+    if viewingFavorites {
+      title = "Unfavorite"
+    }
+    else if let fratFavorited = fraternities[self.fratNames[indexPath.row]]?.getProperty(named: "favorited") as? Bool {
+        if fratFavorited { title = "Unfavorite" }
+    }
+    let toggleFavorite = UITableViewRowAction(style: .normal, title: title, handler: {
+        action, index in
+        var fratName = self.fratNames[index.row]
+        if (self.viewingFavorites) {
+          fratName = self.favoritedFrats[index.row]
+        }
+        let favoritedFrat = self.fraternities[fratName]!
+        favoritedFrat.setProperty(named: "favorited", to: action.title == "Favorite")
+        if (action.title == "Unfavorite") {
+          if let fratIndex = self.favoritedFrats.index(of: favoritedFrat.name) {
+            self.favoritedFrats.remove(at: fratIndex)
+            if (self.viewingFavorites){
+              self.tableView.deleteRows(at: [index], with: UITableViewRowAnimation.left)
+            }
+          }
+        }
+        else {
+          self.favoritedFrats.append(favoritedFrat.name)
+        }
+        
+    })
+    if (title == "Favorite"){
+      toggleFavorite.backgroundColor = COLOR_CONST.MENU_COLOR
+    }
+    else {
+      toggleFavorite.backgroundColor = COLOR_CONST.MENU_COLOR.withAlphaComponent(0.5)
+    }
+    
+    return [toggleFavorite]
+  }
+  
+  
+  
+  
+  
+  
+  
 }
+
+
+
+
+
+
+
+
 
