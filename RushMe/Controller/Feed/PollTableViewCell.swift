7ebbf7027f21329c6e7f7f0d420c0d7f210a88e2
Adam K <adamthk@gmail.com>
2018-02-23 13:19:02 -0500

    Poll UI Practically Complete

--- RushMe/Controller/Feed/PollTableViewCell.swift
+++ RushMe/Controller/Feed/PollTableViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PollTableViewCell: UITableViewCell {
-  let options = ["Saturday", "Sunday", "Monday"]
+  var options = ["Saturday", "Sunday", "Monday"]
   
   @IBOutlet weak var pollFraternityLabel: UILabel!
   @IBOutlet weak var pollTitleLabel: UILabel!
@@ -21,15 +21,21 @@ class PollTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     let margins = self.layoutMarginsGuide
+    //self.translatesAutoresizingMaskIntoConstraints = false
+    self.contentView.translatesAutoresizingMaskIntoConstraints = false
+    if options.count == 0 {
+     options = ["No Options"] 
+    }
     var previousButton : UIButton? = nil
     for option in options {
       let button = UIButton(type: .roundedRect)
-      button.backgroundColor = RMColor.AppColor
-      button.setTitleColor(UIColor.white, for: .normal)
+      button.backgroundColor = UIColor.clear//RMColor.AppColor
+      button.setTitleColor(RMColor.AppColor, for: .normal)
       button.translatesAutoresizingMaskIntoConstraints = false
       button.setTitle(option, for: .normal)
+      button.addTarget(self, action: #selector(PollTableViewCell.selected(_:)), for: .touchUpInside)
       self.addSubview(button)
-      button.heightAnchor.constraint(equalToConstant: 32)
+      button.heightAnchor.constraint(equalToConstant: 32).isActive = true
       
       button.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 8).isActive = true
       button.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: -8).isActive = true
@@ -37,18 +43,22 @@ class PollTableViewCell: UITableViewCell {
         button.topAnchor.constraint(equalTo: pButton.layoutMarginsGuide.bottomAnchor, constant: 8).isActive = true
       }
       else {
-        button.topAnchor.constraint(equalTo: pollDescriptionTextView.layoutMarginsGuide.bottomAnchor, constant: 8).isActive = true
+        button.topAnchor.constraint(equalTo: pollDescriptionTextView.bottomAnchor, constant: 8).isActive = true
       }
       previousButton = button
     }
     self.contentView.bottomAnchor.constraint(equalTo: previousButton!.bottomAnchor, constant: 8).isActive = true
-
+    self.contentView.heightAnchor.constraint(equalToConstant: 128 + CGFloat(options.count)*40).isActive = true
+    //self.heightAnchor.constraint(equalToConstant: 256).isActive = true
     self.layoutIfNeeded()
     
     //self.bottomAnchor.constraint(equalTo: previousButton!.bottomAnchor, constant: 8).isActive = true
     
     }
 
+  @objc func selected(_ pollChoice : UIButton) {
+    print(pollChoice.currentTitle)
+  }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
       
