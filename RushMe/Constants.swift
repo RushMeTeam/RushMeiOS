9f48acaabff11c89d754d5a1ca2760965b609a60
Adam K <adamthk@gmail.com>
2018-09-04 17:43:11 -0400

    Make Constants.swift private

--- RushMe/Constants.swift
+++ RushMe/Constants.swift
@@ -3,7 +3,7 @@
 //  RushMe
 //
 //  Created by Adam Kuniholm on 10/8/17.
-//  Copyright © 2017 4 1/2 Frat Boys. All rights reserved.
+//  Copyright © 2017 4  1/2 Frat Boys. All rights reserved.
 //
 
 import Foundation
